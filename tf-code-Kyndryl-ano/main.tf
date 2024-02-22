7dce1593bc859482e422c70b602ecc9bd34bdc42
Anish <cuxtud@gmail.com>
2024-02-22 10:27:29 +0000

    updated

--- tf-code-Kyndryl-ano/main.tf
+++ tf-code-Kyndryl-ano/main.tf
@@ -1,4 +1,11 @@
-
+terraform {
+  required_providers {
+    nutanix = {
+      source  = "nutanix/nutanix"
+      version = "1.9.5"
+    }
+  }
+}
 # zet provider, Nutanix 
 provider "nutanix" {
   username     = var.nutanix_username
